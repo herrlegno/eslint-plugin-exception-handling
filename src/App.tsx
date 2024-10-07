9580cf7c3b4de0b37f467013b6d59828c71a9aae
Alejandro Gonzalez <11634168+herrlegno@users.noreply.github.com>
2024-10-07 14:57:08 +0100

    adds throw functions

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function throwFunction() {
+    throw new Error('throwFunction')
+}
+
+throwFunction()
+
 function App() {
   const [count, setCount] = useState(0)
+
+  throwFunction()
 
   return (
     <>
